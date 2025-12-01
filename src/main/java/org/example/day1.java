2adc79e994a21868b5b5c19a911bff848cf85d12
John Ritsko <jmritsko@gmail.com>
2025-12-01 17:16:45 -0500

    day 1 solved

--- src/main/java/org/example/day1.java
+++ src/main/java/org/example/day1.java
@@ -1,7 +1,99 @@
 package org.example;
 
-public class day1 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
+
+public class day1 {
+    static int pos = 50;
+
+    public static int part1(ArrayList<String> input){
+        int count = 0;
+
+        for (String str : input) {
+            String dir = str.substring(0, 1);
+            int clicks = Integer.parseInt(str.substring(1));
+
+            if (dir.equals("L")){
+                clicks = clicks * -1;
+            }
+
+            pos += clicks;
+
+            while(pos < 0){
+                pos += 100;
+            }
+
+            while(pos > 99){
+                pos -= 100;
+            }
+
+            if (pos == 0){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static int part2(ArrayList<String> input) {
+        int count = 0;
+        int oldPos;
+
+        for (String str : input) {
+            oldPos = pos;
+            String dir = str.substring(0, 1);
+            int clicks = Integer.parseInt(str.substring(1));
+
+            if (dir.equals("L")){
+                clicks = clicks * -1;
+            }
+
+            pos += clicks;
+
+            while(pos < 0){
+                count++;
+                pos += 100;
+            }
+
+            while(pos > 99){
+                if (pos != 100) {
+                    count++;
+                }
+                pos -= 100;
+            }
+
+            if(oldPos == 0 && dir.equals("L")){
+                count--;
+            }
+
+            if(oldPos == 0 && dir.equals("R") && clicks % 100 == 0){
+                count--;
+            }
+
+            if (pos == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> input = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("inputs/input_1"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                input.add(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        int count = part2(input);
+
+        System.out.println(count);
     }
 }
---
+++ src/main/java/org/example/template.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class template {
+
+    public static void main(String[] args) {
+        try {
+            ArrayList<String> input = new ArrayList<>();
+
+            try (BufferedReader br = new BufferedReader(new FileReader("inputs/test_X"))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    input.add(line);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+
+
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
